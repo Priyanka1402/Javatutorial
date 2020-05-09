176c6c89035278791233efc2e2e407ddc1f56088
dillipnayak <dillipnayak@localhost>
2020-05-09 11:15:00 -0400

    sajal's tutorial

---
+++ JavaTutorial/src/main/java/Runner/MyFirst.java
@@ -0,0 +1,32 @@
+package Runner;
+
+public class MyFirst {
+	
+	
+
+	public static void main(String[] args) {
+		
+		
+		
+		if (args.length>0) {
+			for (String arg : args  ) {
+				
+				System.out.println(arg);
+				
+			}
+		}
+		
+		//System.out.println("Theres are "+args.length+" elements in args");
+		
+		sayHello("DIllip");
+
+	}
+	
+	
+	public static void  sayHello(String name) {
+		
+		System.out.println("Hello "+name);
+		
+	}
+
+}
